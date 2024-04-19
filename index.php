32d3ebf45c80b6317f51033e3091c8c7921c27ca
Aefvvges <torri.barbara4@gmail.com>
2024-04-19 16:03:37 -0300

    aver

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido</title>
+</head>
+<body>
+    <?php
+    echo"<h1>Hola {$_GET['nombre']} {$_GET['apellido']}</h1>";
+    ?>
+</body>
+</html>
